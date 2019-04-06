9e024559a5465255e03ca6d02ea9846e7d3fcf8d
Suyog Mishal <suyog15122@gmail.com>
2019-04-07 00:31:50 +0530

    MainPc

--- src/Form/QuestionsFilterType.php
+++ src/Form/QuestionsFilterType.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Query\Expr;
 use Doctrine\ORM\QueryBuilder;
 use Lexik\Bundle\FormFilterBundle\Filter\FilterBuilderExecuterInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,7 +17,9 @@ class QuestionsFilterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('marks')
+            ->add('marks',IntegerType::class,[
+                'required' => false
+            ])
             ->add('chapter',ChaptersFilterType::class,[
                 'add_shared' => function (FilterBuilderExecuterInterface $qbe){
                     $closure = function (QueryBuilder $filterBuilder,$alias,$joinAlias,Expr $expr){
